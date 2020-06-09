adb7ffc51cc09203799c2a0400d5105bf4e74234
Icaro Oliveira <icarovinici@gmail.com>
2020-06-09 00:27:32 -0300

    page on map finished

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Constants from 'expo-constants'
 import { Feather as Icon } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import MapView from 'react-native-maps'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import { SvgUri } from 'react-native-svg'
 
 const Points = () => {
   const navigation = useNavigation()
@@ -12,19 +13,66 @@ const Points = () => {
     navigation.goBack()
   }
 
+  function handleNavigateToDetail() {
+    navigation.navigate('Detail')
+  }
+
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={handleNavigateBack}>
-        <Icon name="arrow-left" size={20} color="#34cb79" />
-      </TouchableOpacity>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
 
-      <Text style={styles.title}>Bem vindo.</Text>
-      <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+        <Text style={styles.title}>Bem vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
 
-      <View style={styles.mapContainer}>
-        <MapView style={styles.map} />
+        <View style={styles.mapContainer}>
+          <MapView style={styles.map} initialRegion={{
+            latitude: -7.3273758,
+            longitude: -35.3356511,
+            latitudeDelta: 0.014,
+            longitudeDelta: 0.014,
+          }}>
+            <Marker style={styles.mapMarker} onPress={handleNavigateToDetail} coordinate={{
+              latitude: -7.3273758,
+              longitude: -35.3356511,
+            }}>
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={{ uri: "https://www.gestaodeclientes.com.br/wp-content/uploads/2019/08/supermercado-de-bairro.jpg" }} />
+                <Text style={styles.mapMarkerTitle}>Mercado</Text>
+              </View>
+            </Marker>
+          </MapView>
+        </View>
       </View>
-    </View>
+
+      <View style={styles.itemsContainer}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{
+          paddingHorizontal: 20
+        }}>
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </>  
   )
 }
 
--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -10,9 +10,6 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        //192.168.0.109 - expo
-        //or
-        //localhost
         image_url: `http://192.168.0.109:3333/uploads/${item.image}`,
       }
     })
